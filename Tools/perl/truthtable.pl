fc33eadade914ae9d663399ced8ac00f501d94ba
Philipp Gühring <pg@futureware.at>
2019-02-22 18:38:23 +0000

    Latex Support for Truthtable

--- Tools/perl/truthtable.pl
+++ Tools/perl/truthtable.pl
@@ -1,14 +1,22 @@
 #!/usr/bin/perl -w
 use strict;
+use Getopt::Long;
+
+our $debug=0;
+our $format="text"; # html latex
+
+GetOptions ("debug" => \$debug,
+	    "v" => \$debug,
+            "format=s" => \$format);
 
 sub bin2gray
 {
     return $_[0] ^ ($_[0] >> 1);
 }
 
-sub verb
+sub verb # verbose debug output
 {
-	#  print $_[0];
+  print $_[0] if($debug);
 }
 
 sub truth
@@ -103,7 +111,7 @@ foreach my $file(@ARGV)
 {
   if(open(IN,"<$file"))
   {
-    print "Analyzing $file\n";
+    print STDERR "Analyzing $file\n";
     my @lines=<IN>;
     close IN;
 
@@ -125,28 +133,139 @@ foreach my $file(@ARGV)
 
     my $ninputs=scalar(keys %inputs); my $combinations=2**$ninputs;
     my $noutputs=scalar(keys %outputs);
-    print "Number of Inputs: $ninputs (".join(",",@ins).") -> Combinations: $combinations\n";
-    print "Number of Outputs: $noutputs (".join(",",@outs).")\n";
+    verb "Number of Inputs: $ninputs (".join(",",@ins).") -> Combinations: $combinations\n";
+    verb "Number of Outputs: $noutputs (".join(",",@outs).")\n";
 
     if($ninputs<1)
     {
       die "ERROR: A cell without an input!\n";
     }
 
-    print join(" ",@ins)."->".join(" ",@outs); print "\n";
+    if($format eq "text")
+    {
+      print join(" ",@ins)."->".join(" ",@outs); print "\n";
+    }
+    elsif($format eq "latex")
+    {
+print <<EOF
+%%  ************    LibreSilicon's StdCellLibrary   *******************
+%%
+%%  Organisation:   Chipforge
+%%                  Germany / European Union
+%%
+%%  Profile:        Chipforge focus on fine System-on-Chip Cores in
+%%                  Verilog HDL Code which are easy understandable and
+%%                  adjustable. For further information see
+%%                          www.chipforge.org
+%%                  there are projects from small cores up to PCBs, too.
+%%
+%%  File:           StdCellLib/Documents/LaTeX/truthtable_AOI21.tex
+%%
+%%  Purpose:        Truth Table File for AOI21
+%%
+%%  ************    LaTeX with circdia.sty package      ***************
+%%
+%%  ///////////////////////////////////////////////////////////////////
+%%
+%%  Copyright (c) 2018 by chipforge <hsank\@nospam.chipforge.org>
+%%  All rights reserved.
+%%
+%%      This Standard Cell Library is licensed under the Libre Silicon
+%%      public license; you can redistribute it and/or modify it under
+%%      the terms of the Libre Silicon public license as published by
+%%      the Libre Silicon alliance, either version 1 of the License, or
+%%      (at your option) any later version.
+%%
+%%      This design is distributed in the hope that it will be useful,
+%%      but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+%%      See the Libre Silicon Public License for more details.
+%%
+%%  ///////////////////////////////////////////////////////////////////
+
+\\begin{center}
+EOF
+;
+
+print "    {\(Z = \\lnot ((B1 \\land B0) \\lor A) \\)}\n";
+print "    \\begin{table}[h] %\\caption{\\(Z = \\lnot ((B1 \\land B0) \\lor A) \\)}\n";
+print "        \\begin{center}\n";
+print "            \\begin{tabular}{";
+print "|c" foreach(@ins);
+print "|";
+print "|c" foreach(@outs);
+print "|} \\hline\n";
+print "            "; print join(" & ",@ins)." & ".join(" & ",@outs)." \\\\ \\hline\\hline\n";
+    }
+    elsif($format eq "html")
+    {
+      print "<table border='1'><th>".join("</th><th>",@ins)."</th><th><b>".join("</b></th><th><b>",@outs)."</b></th></tr>";
+    }
     my %values=();
+    our %sum=();
+    our %results=();
     foreach my $i(0 .. 2**$ninputs-1)
     {
       my $gray=bin2gray($i); 
+      print "            " if($format eq "latex");
       foreach(0 .. $ninputs-1)
       {
+	print "& " if($format eq "latex" && $_>0);
         print "".($gray&(1<<$_))?"1 ":"0 ";
 	$values{$ins[$_]}=($gray&(1<<$_))?1:0;
       }
       my %res=truth(\@lines,\%values);
-      print "$_=$res{$_} " foreach(@outs); print "\n";
+      
+      foreach my $out (@outs)
+      {
+        $sum{$out}{$res{$out}}++;
+	my @a=();
+	foreach(@ins)
+	{
+          push @a,$res{$_}?"$_":"!$_";
+	}
+	push @{$results{$out}{$res{$out}}},join(" && ",@a);
+      }
+
+      if($format eq "text")
+      {
+        print "$_=$res{$_} " foreach(@outs); 
+      }
+      elsif($format eq "latex")
+      {
+        print "& $res{$_} " foreach(@outs);
+      }
+      elsif($format eq "html")
+      {
+        print "<td><b>$res{$_}</b></td>" foreach(@outs);
+      }
+      print "\\\\ \\hline" if($format eq "latex");
+      print "\n";
     }
-    print "\n";
+      foreach my $out (@outs)
+      {
+        my $not=$sum{$out}{0}>$sum{$out}{1}?1:0;
+	if($not)
+	{
+          print "FUNCTION: $out = (".join(" || ",@{$results{$out}{$not}}).")";
+	}
+	else
+	{
+          print "FUNCTION: $out = ! (".join(" || ",@{$results{$out}{$not}}).")";
+        }
+      }
+
+    print "\n" if($format eq "text");
+    if($format eq "latex")
+    {
+	    print <<EOF
+            \\end{tabular}
+        \\end{center}
+    \\end{table}
+\\end{center}
+EOF
+;
+    }
   }
 }
-print "Done.\n";
+print STDERR "Done.\n";
