a0c65bcab2808df2f71a6c4909aaeee8621f7466
chipforge <popcorn@nospam.chipforge.org>
2019-04-25 16:37:11 +0200

    [CELLS] Add still handcrafted Switch Model and Stimulus for XOR2 as Popcorn feed

--- Sources/verilog/XOR2_switch.v
+++ Sources/verilog/XOR2_switch.v
@@ -11,13 +11,13 @@
 //
 //  File:           StdCellLib/Sources/verilog/XOR2_switch.v
 //
-//  Purpose:        XOR2 switch-level model
+//  Purpose:        Verilog switch-level model
 //
-//  ************    IEEE Std 1364-2001 (Verilog HDL)    ***************
+//  ************    IEEE Std 1364-1995 (Verilog '95 HDL)    ***********
 //
 //  ///////////////////////////////////////////////////////////////////
 //
-//  Copyright (c) 2018 by chipforge <hsank@nospam.chipforge.org>
+//  Copyright (c) 2019 by chipforge <stdcelllib@nospam.chipforge.org>
 //  All rights reserved.
 //
 //      This Standard Cell Library is licensed under the Libre Silicon
@@ -34,37 +34,35 @@
 //  ///////////////////////////////////////////////////////////////////
 
 //  -------------------------------------------------------------------
-//                          SWITCH-LEVEL MODEL
+//                  SWITCH-LEVEL MODEL
 //  -------------------------------------------------------------------
 
-module XOR2_switch (
-    output              Z,
-    input               B,
-    input               A
-);
+module XOR2_switch (Z, B, A);
+
+    output          Z;
+    input           B, A;
 
 //  ------------    1st stage - NOR2    -------------------------------
 
-    wire                y;              // result after 1st stage
-    wire                p_ba;           // wire between M0PA & M0PB
+    wire            y;                  // result after 1st stage
+    wire            n1;                 // wire between M0PA & M0PB
 
-pmos M0PA (y, p_ba, A);
-pmos M0PB (p_ba, 'b1, B);
+pmos M0PA (y, n1, A);
+pmos M0PB (n1, 'b1, B);
 
 nmos M0NA (y, 'b0, A);
 nmos M0NB (y, 'b0, B);
 
 //  ------------    2nd stage - AOI21   -------------------------------
 
-    wire                p_0ba;          // wire between M1P0, M1PA & M1PB
-    wire                n_ba;           // wire between M1NB & M1NA
+    wire            n2, n3;
 
-pmos M1P0 (Z, p_0ba, y);
-pmos M1PA (p_0ba, 'b1, A);
-pmos M1PB (p_0ba, 'b1, B);
+pmos M1P0 (Z, n3, y);
+pmos M1PA (n3, 'b1, A);
+pmos M1PB (n3, 'b1, B);
 
 nmos M1N0 (Z, 'b0, y);
-nmos M1NA (Z, n_ba, A);
-nmos M1NB (n_ba, 'b0, B);
+nmos M1NA (Z, n2, A);
+nmos M1NB (n2, 'b0, B);
 
 endmodule
