a8ab20ef0b9673392aea76b1dec89a0fcbe6c124
Philipp Gühring <pg@futureware.at>
2019-07-09 16:44:37 +0000

    Moved the tech files to the Tech directory

--- Tools/perl/librecells.pl
+++ Tools/perl/librecells.pl
@@ -12,11 +12,11 @@ while(<IN>)
     my $cellname=$1;	   
     my $placer=""; $placer="--placer=hierarchical" if($cellname=~m/^(CLK|DFF|FAX|HAX)/);
     next if($cellname eq "CLKBUF3");
-    my $cmd="lclayout --output-dir outputlib --tech librecell_tech.py --netlist $sp --cell $cellname -v $placer";
+    my $cmd="lclayout --output-dir outputlib --tech ../Tech/librecell_tech.py --netlist $sp --cell $cellname -v $placer";
     print "$cmd\n";
     system $cmd;
 
-    open OUT,"|magic -dnull -noconsole -T libresilicon >/dev/null 2>/dev/null";
+    open OUT,"|magic -dnull -noconsole -T ../Tech/libresilicon >/dev/null 2>/dev/null";
     print OUT <<EOF
 drc off
 box 0 0 0 0
