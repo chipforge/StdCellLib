403203861e93d694794ff589a673c564b470ca9c
Philipp Gühring <pg@futureware.at>
2019-07-15 08:41:20 +0000

    Added debug output control

--- Tools/perl/librecells.pl
+++ Tools/perl/librecells.pl
@@ -2,6 +2,8 @@
 
 mkdir "outputlib";
 
+my $debug=0;
+
 my $sp=$ARGV[0]||"libresilicon.sp";
 
 open IN,"<$sp";
@@ -11,12 +13,12 @@ while(<IN>)
   {
     my $cellname=$1;	   
     my $placer=""; $placer="--placer=hierarchical" if($cellname=~m/^(CLK|DFF|FAX|HAX)/);
-    next if($cellname eq "CLKBUF3");
+    next if($cellname eq "CLKBUF3");# TODO: CLKBUF3 currently takes too much time to generate so we have to skip it, librecell needs to be improved
     my $cmd="lclayout --output-dir outputlib --tech ../Tech/librecell_tech.py --netlist $sp --cell $cellname -v $placer";
     print "$cmd\n";
     system $cmd;
 
-    open OUT,"|magic -dnull -noconsole -T ../Tech/libresilicon >/dev/null 2>/dev/null";
+    open OUT,"|magic -dnull -noconsole -T ../Tech/libresilicon ".($debug?"":">/dev/null 2>/dev/null");
     print OUT <<EOF
 drc off
 box 0 0 0 0
