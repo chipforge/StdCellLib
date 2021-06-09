d570162a392535af4dd70d288d1624220e95d518
Philipp Gühring <pg@futureware.at>
2021-06-10 01:21:46 +0200

    Changed correction naming convention

--- Tools/perl/drcfixall.pl
+++ Tools/perl/drcfixall.pl
@@ -9,6 +9,7 @@ my $tech=$ARGV[0] || "../Tech/libresilicon.tech";
 
 foreach my $magfile (<*.mag>)
 {
+  next if($magfile=~m/^corr_/); # Ignore correction files
   my $cellname=$magfile; $cellname=~s/\.mag$//;	
 
   step("NEXT STEP: DRC Check with Magic");
@@ -20,15 +21,15 @@ foreach my $magfile (<*.mag>)
   step("NEXT STEP: DRC Check with Magic");
   system "../Tools/perl/drccheck.pl $cellname.mag $tech";
 
-  if(-f "corr.$cellname.mag")
+  if(-f "corr_$cellname.mag")
   {
     unlink "$cellname.mag";
-    rename "corr.$cellname.mag","$cellname.mag";
+    rename "corr_$cellname.mag","$cellname.mag";
     print "DRC errors in $cellname corrected. Now running final DRC check:\n";
     step("NEXT STEP: Final DRC check");
     system "../Tools/perl/drccheck.pl $cellname.mag $tech";
   }
 
-  exit;
+  #  exit;
 }
 
